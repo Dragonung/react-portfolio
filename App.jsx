7c3c749da65ab78497ff5e5a8150f31f2707072e
Daniel Chen <danielchenishere@gmail.com>
2024-11-18 13:12:45 -0800

    set up main & app .jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import //main component;
+
+function App() {
+  return (
+    <div className="main component">
+      <// main component //>
+    </div>
+  );
+}
+
+export default App;
